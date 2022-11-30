67fd2ae454bc4c5247396b4014fd0575cb837d0f
Ricardo Castellanos <55893622+castellanos-ricardo@users.noreply.github.com>
2022-11-30 14:39:35 -0800

    Time: 100 ms (65.40%), Space: 14.3 MB (69.14%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        dic = {}
+        
+        for i,n in enumerate(nums):
+            if target - n  in dic:
+                return [dic[target-n], i]
+            dic[n] = i
+        
